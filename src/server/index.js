999e11674d0d20238133f527c80a26295884ff9c
chris <chris@republicbjj.com>
2020-06-22 22:06:02 -0600

    fixed build-prod

--- src/server/index.js
+++ src/server/index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 const cors = require('cors');
 app.use(cors());
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static('dist'));
 
 
 app.get('/', function (req, res) {
